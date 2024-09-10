6709091ad7fc945744a6abf7e62f0e5db9b5e765
xKhanDev <safeerkhanxofficial@gmail.com>
2024-09-11 04:45:19 +0500

    add nochatselected conponent

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Messages from "./Messages";
 import MessageInput from "./MessageInput";
+import { TiMessages } from "react-icons/ti";
 
 const MessageContainer = () => {
   return (
@@ -19,3 +20,16 @@ const MessageContainer = () => {
 };
 
 export default MessageContainer;
+
+// If no chat is selected
+export const NoChatSelected = () => {
+  return (
+    <div className="flex items-center justify-center w-full h-full">
+      <div className="px-4 text-center sm:text-lg md:text-lg text-gray-200 font-semibold flex flex-col items-center gap-2">
+        <p>👋 Safeer Khan </p>
+        <p>Select a chat to start messaging</p>
+        <TiMessages className="text-3xl md:text-6xl text-center" />
+      </div>
+    </div>
+  );
+};
