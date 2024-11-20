f036e06c86727576ad761d1d50d345b0367d7638
xKhanDev <safeerkhanxofficial@gmail.com>
2024-11-20 20:54:42 +0500

    some typo

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -8,18 +8,24 @@ const protectRoute = async (req, res, next) => {
       return res.status(401).json({ error: "Unauthorized: No token provided" });
     }
 
-    const decodeToken = jwt.verify(token, process.env.JWT_SECRET_KEY);
-    if (!decodeToken) {
-      return res.status(401).json({ error: "Unauthorized: Invalid token" });
-    }
+    try {
+      const decodeToken = jwt.verify(token, process.env.JWT_SECRET_KEY);
+      const user = await User.findById(decodeToken.userId).select("-password");
 
-    const user = await User.findById(decodeToken.userId).select("-password");
-    if (!user) {
-      return res.status(404).json({ error: "User not found" });
-    }
+      if (!user) {
+        return res.status(404).json({ error: "User not found" });
+      }
 
-    req.user = user;
-    next();
+      req.user = user;
+      next();
+    } catch (error) {
+      if (error.name === "TokenExpiredError") {
+        return res.status(401).json({ error: "Unauthorized: Token expired" });
+      }
+
+      console.error("Error in protectRoute:", error.message);
+      res.status(500).json({ error: "Internal server error" });
+    }
   } catch (error) {
     console.error("Error in protectRoute:", error.message);
     res.status(500).json({ error: "Internal server error" });
